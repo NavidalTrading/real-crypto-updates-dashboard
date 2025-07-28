2c95e11c024842ecf30e4e458a935b5b0ef4d1e2
NavidalTrading <111570404+NavidalTrading@users.noreply.github.com>
2025-07-28 12:40:47 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
 import streamlit.components.v1 as components
 
 # Initialize login state
